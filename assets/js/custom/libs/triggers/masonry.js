bc403e08efca21dd66f4abca0766c0c2d38ac210
Michał Białkowski <michal@bialkowski.net.pl>
2022-09-13 12:53:38 +0200

    Update

--- assets/js/custom/libs/triggers/masonry.js
+++ assets/js/custom/libs/triggers/masonry.js
@@ -1,5 +1,32 @@
+function runMasonry() {
+    $('.masonry').masonry({
+		itemSelector: '.brick',
+        percentPosition: true
+	})
+}
+
 $(window).on('load', function() {
-	$('.masonry').masonry({
-		itemSelector: '.brick'
-	});
+	runMasonry()
+});
+
+let accordions = document.getElementsByClassName('accordion')
+
+for (var i=0; i < accordions.length; i++) {
+    accordions[i].addEventListener('shown.bs.collapse', function() {
+        runMasonry()
+    });
+    
+    accordions[i].addEventListener('hidden.bs.collapse', function() {
+        runMasonry()
+    })
+}
+
+$(function() {
+    $(".masonry").each(function() {
+        $(this).find('.brick').each(function(i) {
+            $(this).css({
+                zIndex: $(this).parent().find('.brick').length - i
+            });
+        });
+    });
 });
